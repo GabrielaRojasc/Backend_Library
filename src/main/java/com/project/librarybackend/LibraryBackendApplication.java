59d79dafb03d48f81c7654b33605552150634548
GabrielaRojasc <gabriela.rojas.chavaco@hotmail.com>
2023-04-04 13:35:21 -0500

    Se agrega la entidad scan para que se ejecute bien con la autenticación

--- src/main/java/com/project/librarybackend/LibraryBackendApplication.java
+++ src/main/java/com/project/librarybackend/LibraryBackendApplication.java
@@ -2,9 +2,12 @@ package com.project.librarybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.project.librarybackend.models"})
+@SpringBootApplication
 public class LibraryBackendApplication {
 
     public static void main(String[] args) {
